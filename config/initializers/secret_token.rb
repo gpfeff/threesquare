b386c555469320686cb64d9ed08df0d7f6958bac
Greg Pfeffer <greg.a.pfeffer@gmail.com>
2013-12-04 14:47:51 -0600

    Added database and Locations table

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Threesquare::Application.config.secret_key_base = 'b029306b5f85a9a2714e4c52257b216a6b22c24aa0d85cbf51610c823f8268e83f339590d9239ceb6f193b478df38a9203f3df2308b02a32d170dc1a8be1013f'
